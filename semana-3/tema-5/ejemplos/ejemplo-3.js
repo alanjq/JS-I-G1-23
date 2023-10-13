b46a412c2e614e659257e507d4728ac4cce1bd9e
Alan Jimenez <alanjq@outlook.com>
2023-10-12 20:35:36 -0500

    Ejemplo Sesión 5

---
+++ semana-3/tema-5/ejemplos/ejemplo-3.js
@@ -0,0 +1,41 @@
+class Moneda {
+    _valor
+    _codigo
+
+    constructor(valor, codigo) {
+        this._valor = valor
+        this._codigo = codigo
+    }
+
+    getTipoCambio(cantidad) {
+        return this._valor * cantidad
+    }
+}
+
+class Euro extends Moneda {
+    constructor() {
+        super(.053, 'EUR')
+    }
+}
+
+class PesoMexicano extends Moneda {
+    constructor() {
+        // Llama al constructor de la clase de la que heredamos
+        // new Moneda()
+        super(1, 'MXN')
+    }
+
+    convertirAEuro(cantidad) {
+        const EURO = new Euro()
+        return (EURO._valor * this._valor * cantidad) + " " + EURO._codigo
+    }
+}
+
+
+
+const pesoMXN = new PesoMexicano()
+pesoMXN._codigo // ?
+pesoMXN.convertirAEuro(100) // ?
+
+const euro = new Euro()
+euro._codigo // ?
