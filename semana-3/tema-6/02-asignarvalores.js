4d84010940037b668c7064fbf7df1e2f3afccab2
Alan Jimenez <alanjq@outlook.com>
2023-10-17 20:16:09 -0500

    Ejercicio

--- semana-3/tema-6/02-asignarvalores.js
+++ semana-3/tema-6/02-asignarvalores.js
@@ -13,24 +13,6 @@ function funcionMap(valor) {
 
 [1, 2, 3, 4].map(funcionMap)
 
-// Función asíncrona
-async function ejemplo() {
-    setTimeout(() => {
-        return "ok"
-    }, 1000)
-}
-
-ejemplo()
-    .then(() => {
-        console.log('OK');
-    })
-    .catch(() => {
-        console.error('error');
-    })
-
-
-console.log('algo extra');
-
 
 // Funciones puras e impuras
 let fecha = () => Date.now
@@ -56,3 +38,45 @@ tiempo();//?
 const suma = (a, b) => (a + b)
 suma(2, 5) // ?
 suma(2, 5) // ?
+
+
+// Cambio de valor en variable global
+// window - alcance/scope global/padre
+var variable_global = 5 // window.variable_global
+
+// Hereda directamente el alcance/scope de window
+const funcion = x => { // window.funcion
+    // this = window
+    variable_global = 0
+}
+
+// Crea su propio alcance/scope como hijo de window
+function ejemplo() { // window.ejemplo
+    // this = ejemplo
+    variable_global
+    // No se puede acceder a "a" desde afuera de este metodo
+    return function a() {
+        console.log('SCOPE', this);
+        return 'funcion dentro de ejemplo'
+    }
+}
+
+
+// Strings inmutables
+//  012345678901234567890123
+const cadena = "Este string es inmutable"
+const cadena1 = cadena.slice(5, 11)
+const cadena2 = cadena.toUpperCase()
+
+cadena
+cadena1
+cadena2
+
+// Arreglos mutables
+const arreglo = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+const nuevoArreglo = { ...arreglo, c: 4, d: 5 }
+nuevoArreglo
---
+++ semana-3/tema-6/actividad/ejercicio/js/main.js
@@ -0,0 +1,60 @@
+const compose = (...functions) => data =>
+    functions.reduceRight((value, func) => func(value), data)
+
+let descripcion = document.getElementById('descripcion')
+let calorias = document.getElementById('calorias')
+let carbohidratos = document.getElementById('carbohidratos')
+let proteina = document.getElementById('proteina')
+
+// Agregar función val
+HTMLInputElement.prototype.isEmpty = function () {
+    return this.value.length === 0 ? true : false
+}
+
+const validar = (input) => {
+    let element = document.getElementById(input)
+    // Opción 1 - más corta
+    // element.isEmpty() ? element.classList.add('is-invalid') : element.classList.remove('is-invalid')
+
+    // Opción 2 - más clara
+    if (element.isEmpty()) {
+        element.classList.add('is-invalid')
+    } else {
+        element.classList.remove('is-invalid')
+    }
+}
+
+function validarInput() {
+    validar('descripcion')
+    validar('calorias')
+    validar('carbohidratos')
+    validar('proteina')
+}
+
+
+function agregar() {
+    // let nuevoElemento = {
+    //     descripcion: descripcion.value,
+    //     calorias: calorias.value,
+    //     carbohidratos: carbohidratos.value,
+    //     proteina: proteina.value,
+    // }
+
+    let allInputs = document.querySelectorAll('input')
+    allInputs.forEach((inputelement) => {
+        if (inputelement.value == '') {
+            validarInput()
+            return;
+        }
+    })
+
+    // Sacar HTML de la tabla
+    let tbodyHTMl = document.querySelector('table-body')
+    let nuevoHtml = `${tbodyHTMl.innerHTML}<tr>
+        <td>${descripcion.value}</td>
+        <td>${calorias.value}</td>
+        <td>${proteina.value}</td>
+    </tr>`;
+
+    tbodyHTMl.innerHTML = nuevoHtml
+}
