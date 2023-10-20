90887885d81179c0af053fbd218755633e8fdada
Alan Jimenez <alanjq@outlook.com>
2023-10-19 22:18:13 -0500

    Actividad

--- semana-4/tema-8/actividad/actividad.js
+++ semana-4/tema-8/actividad/actividad.js
@@ -17,3 +17,24 @@ const boton_guardar = document.getElementById('btnGuardar')
 boton_guardar.addEventListener('click', () => {
     titulo_documento.innerText = input_titulo_documento.value
 })
+
+// Cambiar el título a la par que cambia el input
+const todos_los_inputs = document.querySelectorAll('input')
+todos_los_inputs.forEach((un_input) => {
+    // Título documento
+    if (un_input.getAttribute('id') == 'inputTituloDocumento') {
+        // Agregar evento onchange
+        un_input.addEventListener('keyup', () => {
+            document.getElementById('tituloDocumento').innerText = un_input.value
+        })
+    }
+    // Título pestaña
+    if (un_input.getAttribute('id') == 'inputTituloVentana') {
+        // Agregar evento onchange
+        un_input.addEventListener('keyup', () => {
+            document.title = un_input.value
+        })
+    }
+})
+
+
